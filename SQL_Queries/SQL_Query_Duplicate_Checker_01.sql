66d701e3e87545c9e1945a991baed64e513757ac
staid03 <staid03@gmail.com>
2017-04-09 13:22:31 +1000

    Adding scripts for querying the database

---
+++ SQL_Queries/SQL_Query_Duplicate_Checker_01.sql
@@ -0,0 +1,12 @@
+--Query for extracting duplicate files from the database
+--
+-- Version	Date		Author 			Notes
+--	0.1		09-APR-2017	Staid03			Initial
+--
+
+select a.FileName, b.FileName, a.MD5Checksum, a.FileLocation, b.FileLocation
+from Files a, Files b
+where
+a.MD5Checksum = b.MD5Checksum and
+a.FileName <> b.FileName and
+a.MD5Checksum <> ""
